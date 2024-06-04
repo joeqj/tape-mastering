3a8665ae9d958f023d8c57ecba8c15bcaea51fce
joeqj <heyimjoeqj@gmail.com>
2024-06-04 17:46:33 +0100

    Update wording

--- resources/views/user/upload.blade.php
+++ resources/views/user/upload.blade.php
@@ -74,7 +74,7 @@
                 <p x-show="hasError" x-text="error" class="text-red-500 mt-2" x-cloak></p>
             </div>
 
-            <p>Currency conversion will be taken care of in the next step</p>
+            <p>Currency conversion will take place in the next step</p>
 
             <button type="submit"
                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Go
